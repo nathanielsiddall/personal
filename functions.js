92ef28000b3e31c6c16bb97f4b836ac08ae5f5d6
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-03-18 13:45:31 -0500

    made a real simple javascript popup function. it was dumb but it was funny annoying my wife with it.

---
+++ functions.js
@@ -0,0 +1,28 @@
+
+function guessWhat() {
+    alert("Guess what?");
+
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+
+
+
+
+
+}
+
+
+
+guessWhat();
