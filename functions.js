789b58dd3af9fd92463f88a2756b42c1c426f636
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-03-22 13:19:47 -0500

    tried to play with js. it didnt go... at all

--- functions.js
+++ functions.js
@@ -14,15 +14,22 @@ function guessWhat() {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
 
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+// myFunction();
 
-
-
-
-
-
+function doTheThing() {
+    var x = document.getElementById("theDiv");
+    console.log(x);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
 
-
-guessWhat();
